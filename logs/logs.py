7a09fdefc1616ef77390ee88327fa4d078be18a9
Raven Robertson <ravewillow6383@gmail.com>
2019-10-17 14:02:36 -0700

    still wrking on alphabatizing

--- logs/logs.py
+++ logs/logs.py
@@ -1,4 +1,22 @@
 def reorder_logs(logs):
-    logs.split(' ')
-    print(logs)
-   
+    digs = []
+    alpha = []
+    for item in logs:
+        lst = [char for char in item]
+        if lst[-1].isdigit():
+            digs.append(item)
+        else:
+            alpha.append(item[1:])
+    
+
+            
+            print(lst)
+
+
+    logs.clear()
+    logs = alpha + digs
+
+    return logs
+
+logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+reorder_logs(logs)
--- logs/test_logs.py
+++ logs/test_logs.py
@@ -6,5 +6,4 @@ def test_exists():
 def test_vanilla():
     logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
 
-
     assert reorder_logs(logs) == ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
